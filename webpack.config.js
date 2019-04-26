ef5d9fe8610b823b9e297c1aba486b7fbd028034
piperpi <45557041+piperpi@users.noreply.github.com>
2019-04-26 17:06:10 +0800

    4.26

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 // 1. 导入 在内存中生成页面的webpack插件
 const htmlWebpackPlugin = require('html-webpack-plugin')
-
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 // 使用Node语法，向外暴露配置对象，从而，让webpack运行的时候，加载指定的配置
 // 为什么可以使用Node语法？因为 webpack 这个工具，就是基于node构建的；
 module.exports = {
@@ -14,7 +14,8 @@ module.exports = {
         new htmlWebpackPlugin({ // 创建一个 htmlWebpackPlugin 的实例对象
             template: path.join(__dirname, './src/index.html'), // 指定模板页面路径
             filename: 'index.html' // 指定内存中生成的HTMl文件名称
-        })
+        }),
+        new VueLoaderPlugin()
     ],
     module: { // 用来配置 非JS文件对应的loader的
         rules: [ // 就是这些 非 JS 文件 和 loader 之间的对应关系
